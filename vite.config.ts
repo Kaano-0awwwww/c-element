b2a0ba031685c04e8ca5f3dcb56e66147937186d
yeLin <1809077526@qq.com>
2025-08-03 03:42:50 +0800

    vitepress

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import eslint from 'vite-plugin-eslint';
-
+import VueMacros from 'vue-macros/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), eslint({ cache: false })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
