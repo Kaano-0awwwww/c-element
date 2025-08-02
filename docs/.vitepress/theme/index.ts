ca7d7362a2989f4e97c8373a700cf555b1466646
yeLin <1809077526@qq.com>
2025-08-03 04:06:57 +0800

    vitepress文档建立

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,17 @@
-import DefaultTheme from 'vitepress/theme';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { ElementPlusContainer } from '@vitepress-demo-preview/component';
+import DefaultTheme from 'vitepress/theme'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { ElementPlusContainer } from '@vitepress-demo-preview/component'
 
-import '@vitepress-demo-preview/component/dist/style.css';
-import '../../../src/styles/index.css';
-import './custom.css';
+import '@vitepress-demo-preview/component/dist/style.css'
+import '../../../src/styles/index.css'
+import './custom.css'
 
-library.add(fas);
+library.add(fas)
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
-    app.component('demo-preview', ElementPlusContainer);
-  },
-};
+    app.component('demo-preview', ElementPlusContainer)
+  }  
+}
--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -4,7 +4,7 @@ import { defineProps, defineOptions, withDefaults, ref } from 'vue';
 import Icon from '../Icons/Icon.vue';
 
 defineOptions({
-  name: 'VKButton'
+  name: 'VKButton',
 });
 withDefaults(defineProps<ButtonProps>(), {
   nativeType: 'button',
@@ -12,16 +12,29 @@ withDefaults(defineProps<ButtonProps>(), {
 
 const _ref = ref<HTMLButtonElement>();
 defineExpose({
-  ref: _ref
+  ref: _ref,
 });
 </script>
 <template>
-  <button ref="_ref" class="vk-button" :class="{ [`vk-button--${type}`]: type, [`vk-button--${size}`]: size, 'is-plain': plain, 'is-round': round, 'is-circle': circle, 'is-disabled': disabled }" :disabled="disabled || loading" :type="nativeType" :autofocus="autofocus">
+  <button
+    ref="_ref"
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled || loading"
+    :type="nativeType"
+    :autofocus="autofocus"
+  >
     <!-------图标--------->
     <Icon icon="spinner" spin v-if="loading"></Icon>
     <Icon :icon="icon" v-if="icon"></Icon>
     <span><slot></slot></span>
   </button>
-</template> 
-<style>
-</style>
+</template>
+<style></style>
